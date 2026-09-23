d44d1c31b685e73f0038c3c864ca9fd0b9c6df50
Lucas Flores <lucassflores42@gmail.com>
2026-09-23 13:26:15 -0300

    some parameters change

--- collision_functions.jl
+++ collision_functions.jl
@@ -2,8 +2,8 @@
 #                           Parameters
 # -----------------------------------------------------------------------------
 const restitution_x = 0.5
-const restitution_y = 0.3
-const restitution_angular = 0.5
+const restitution_y = 0.4
+const restitution_angular = 0.3
 const collision_min_distance = grid_size #* sqrt(2)
 const max_velocity = 50.0
 const max_angular_velocity = 20.0
